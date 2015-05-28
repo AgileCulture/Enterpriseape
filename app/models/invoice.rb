e65c18d4cb4178ff89963c723b932fc07a65220d
Shane <shaneymoran@hotmail.com>
2015-05-28 03:36:22 +0100

    purchases had_many scaffold

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ActiveRecord::Base
   
   belongs_to :employee
+  has_many :purchases
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
 
+  resources :purchases
+
   resources :employees
 
   devise_for :users
